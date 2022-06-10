93bfcf260f243608b66582a9d1ea9ec3390dd92d
Ayush Kumar <ayushsaha111@gmail.com>
2022-06-11 01:18:58 +0530

    mongoDB setup - planets

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,28 +1,25 @@
+// const launches = require('./launches.mongo');
+
 const launches = new Map();
 
 let latestFlightNumber = 100;
-
 const launch = {
   flightNumber: 100,
-  mission: "Kepler Exploration X",
-  rocket: "Explorer IS1",
-  launchDate: new Date("December 27, 2030"),
-  target: "Kepler-422 b",
-  customer: ["ZTM", "NASA"],
+  mission: 'Kepler Exploration X',
+  rocket: 'Explorer IS1',
+  launchDate: new Date('December 27, 2030'),
+  target: 'Kepler-442 b',
+  customers: ['ZTM', 'NASA'],
   upcoming: true,
   success: true,
 };
-
 launches.set(launch.flightNumber, launch);
-
-function existsLaunchWithId(launchId){
-  return launches.has(launchId)
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
 }
-
 function getAllLaunches() {
   return Array.from(launches.values());
 }
-
 function addNewLaunch(launch) {
   latestFlightNumber++;
   launches.set(
@@ -30,20 +27,17 @@ function addNewLaunch(launch) {
     Object.assign(launch, {
       success: true,
       upcoming: true,
-      customers: ["Zero to Mastery", "NASA"],
+      customers: ['Zero to Mastery', 'NASA'],
       flightNumber: latestFlightNumber,
     })
   );
 }
- 
-function abortLaunchById(launchId){
-  const aborted = launches.get(launchId)
-  aborted.upcoming = false
-  aborted.success = false
-  return aborted
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
 }
-
-
 module.exports = {
   existsLaunchWithId,
   getAllLaunches,
